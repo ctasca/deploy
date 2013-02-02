9f5497f31023b44b4636e5a958a93ca597c1ad87
Carlo Tasca <carlo@gpmd.co.uk>
2013-02-02 10:21:01 +0000

    added mage:install task

--- magento/mage-deploy.rb
+++ magento/mage-deploy.rb
@@ -4,10 +4,42 @@ load 'config/project.rb'
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 namespace :mage do
+     desc "Install Magento"
+    task :install, :roles => :web do
+        run <<-CMD
+        cd #{magedir} ; curl -O http://www.magentocommerce.com/downloads/assets/#{mage_version}/magento-#{mage_version}.tar.gz &> /dev/null  ;
+        tar -zxvf magento-#{mage_version}.tar.gz ;
+        mv magento/* magento/.htaccess . ;
+        chmod -R o+w media  ;
+        chmod o+w var var/.htaccess app/etc  ;
+        rm -rf magento/ magento-#{mage_version}.tar.gz;
+        mkdir vendor;
+        touch app/etc/local.xml.stage ;
+        touch app/etc/local.xml.live ;
+        CMD
+    end
+    desc "Import data.sql"
+    task :lnsmediavar, :roles => :db do
+        run <<-CMD
+        cd #{magedir} ;
+        cp -R media ../ ;
+        rm -rf media ;
+        ln -s #{projectdir}media media ;
+        cp -R var ../ ;
+        rm -rf var;
+        ln -s #{projectdir}var var ;
+        CMD
+    end
+    task :fork_skeleton, roles => :web do
+        run <<-CMD
+        cd #{magedir} ;
+        ssh git@git.gpmd.net fork site/skeleton/magento/#{mage_version} #{forkto} ;
+        CMD
+    end;
     desc "Install Magento with default data"
     task :install_with_sample, :roles => :web do
         run <<-CMD
-        cd #{deploy_to} ; curl -O http://www.magentocommerce.com/downloads/assets/#{mage_version}/magento-#{mage_version}.tar.gz &> /dev/null ; curl -O http://www.magentocommerce.com/downloads/assets/#{mage_data_version}/magento-sample-data-#{mage_data_version}.tar.gz &> /dev/null ;
+        cd #{magedir} ; curl -O http://www.magentocommerce.com/downloads/assets/#{mage_version}/magento-#{mage_version}.tar.gz &> /dev/null ; curl -O http://www.magentocommerce.com/downloads/assets/#{mage_data_version}/magento-sample-data-#{mage_data_version}.tar.gz &> /dev/null ;
         tar -zxvf magento-#{mage_version}.tar.gz ;
         tar -zxvf magento-sample-data-#{mage_data_version}.tar.gz ;
         mv magento-sample-data-#{mage_data_version}/media/* magento/media/ ;
@@ -21,47 +53,54 @@ namespace :mage do
     desc "Import data.sql"
     task :import_sample_data, :roles => :db do
         run <<-CMD
-        cd #{deploy_to} ;
-        #{mysql_path} -u #{db_user} -p#{db_password} #{db_name} < data.sql ;
+        cd #{magedir} ;
+        #{mysql_path} -u #{db_user} -cd ../p#{db_password} #{db_name} < data.sql ;
         rm data.sql ;
         CMD
     end
     desc "GIT clone"
     task :git_clone, roles => :web do
         run <<-CMD
-        cd #{deploy_to} ;
+        cd #{magedir} ;
         git clone #{repository} . ;
         CMD
     end;
     task :git_init, roles => :web do
         run <<-CMD
-        cd #{deploy_to} ;
+        cd #{magedir} ;
         git init ;
-        git remote add origin #{repository} ;
+        CMD
+    end;
+    task :gitignore, roles => :web do
+        run <<-CMD
+        cd #{magedir} ;
+        rm .gitignore;
+        touch .gitignore ;
+        echo '/media/*' > .gitignore ;
+        echo '/var/*' >> .gitignore ;
+        echo '/vendor/' >> .gitignore ;
+        echo '/app/code/community/Phoenix/' >> .gitignore ;
+        echo '/app/etc/modules/Phoenix_Moneybookers.xml' >> .gitignore ;
+        echo '/app/etc/local.xml' >> .gitignore ;
+        echo '/skin/install/default/default/images/*' >> .gitignore ;
         CMD
     end;
     task :git_commit, roles => :web do
         run <<-CMD
-        cd #{deploy_to} ;
-        rm .gitignore;
-        touch .gitignore ;
-        echo '/downloader/' > .gitignore ;
-        echo '/media/' >> .gitignore ;
-        echo '/var/' >> .gitignore ;
-        echo '/vendor/' >> .gitignore ;
+        cd #{magedir} ;
         git add . ;
         git commit -m "mage installation commit" ;
         CMD
     end;
     task :git_pull_origin, roles => :web do
         run <<-CMD
-        cd #{deploy_to} ;
+        cd #{magedir} ;
         git pull origin #{branch_prompt} ;
         CMD
     end;
     task :git_push_origin, roles => :web do
         run <<-CMD
-        cd #{deploy_to} ;
+        cd #{magedir} ;
         git push origin #{branch_prompt} ;
         CMD
     end;
